254a6a173aaa345ec191e8d69c81d899b69ccb2a
variablemental <346145732@qq.com>
2016-09-27 20:31:01 +0800

    Add files via upload
     adding the graphics of temperature which can describe the temperature tendency of certain City.In the AnalyzeActivity,two graphics will be shown meaning the ranking of cities in the list and the temperature tendency of certain city.
    Next step is to build the logcontentActivity,I have imaged the ability of the logcontent.we can use it to visit the operation system and find what we want to show in this Acitivity,i will construct a path of files to realize it.

--- main/java/com/example/acer/weatherapp/AnalyzeActivity.java
+++ main/java/com/example/acer/weatherapp/AnalyzeActivity.java
@@ -7,9 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import net.sf.json.JSON;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by acer on 2016/9/25.
@@ -18,9 +27,15 @@ public class AnalyzeActivity extends Activity {
     private CityInfo[] src=new CityInfo[10];
     private Button mSelectCity;
     private TextView mTextView;
+    private Button mShowButton;
     public static String REQUEST_OF_ANALYZE="REQUEST_OF_ANALYZE";
+    public static final String REQUEST_OF_LIST="REQUEST_OF_LIST";
+    public static final String REQUEST_OF_CITY="REQUEST_OF_CITY";
+    public static final String JSON_DATE="date";
+    public static final String JSON_INFO="info";
     public static final int requestCode=0;
     private ArrayList<CityInfo> mCitylist=new ArrayList<>();
+    private Map<String,Integer> mTemperature=new HashMap<>();
     Report report=Report.getInstance();
     @Override
     public void onCreate(Bundle onSavedInstanceState) {
@@ -36,10 +51,22 @@ public class AnalyzeActivity extends Activity {
             }
         });
         mTextView=(TextView)findViewById(R.id.analyze_text_city);
+        requestData(REQUEST_OF_LIST);
         TemperatureView tmpv=(TemperatureView)findViewById(R.id.analyze_temperature_city);
-        tmpv.setData(mCitylist);
-        requestData();
-
+        tmpv.setDataForCitys(mCitylist);
+        mShowButton=(Button)findViewById(R.id.analyze_button_show);
+        mShowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTextView.getText()!=null) {
+                    requestData(REQUEST_OF_CITY);
+                    TemperatureView tmpvOfCity=(TemperatureView)findViewById(R.id.analyze_temperature_predict);
+                    tmpvOfCity.setDataForCentainCity(mTemperature);
+                }
+                else
+                    return;
+            }
+        });
     }
 
     @Override
@@ -98,10 +125,29 @@ public class AnalyzeActivity extends Activity {
         mCitylist.add(new CityInfo(new String("重庆")));
         mCitylist.add(new CityInfo(new String("成都")));
     }
-    private void requestData() {
-        for(int i=0;i<mCitylist.size();i++) {
-            mCitylist.get(i).setDetail(report.getRequest(mCitylist.get(i).getCity()));
+    private void requestData(String request) {
+        switch (request) {
+            case REQUEST_OF_LIST:
+            for (int i = 0; i < mCitylist.size(); i++) {
+                mCitylist.get(i).setDetail(report.getRequest(mCitylist.get(i).getCity()));
+            }
+            return;
+            case REQUEST_OF_CITY:
+            String cityname=mTextView.getText().toString();
+            String data=report.getRequest(cityname);
+            final int TOKEN=2;                       //    JSON数据中的"info"JSON数组中的温度数据是第三个
+            try {
+                JSONArray jsonArray=new JSONObject("data").getJSONObject("result").getJSONArray("weather");
+                for(int i=0;i<jsonArray.length();i++) {
+                    String date=jsonArray.getJSONObject(i).getString(JSON_DATE);
+                    Integer temperature=new Integer( jsonArray.getJSONObject(i).getJSONArray(JSON_INFO).getJSONObject(TOKEN).toString());
+                    mTemperature.put(date,temperature);
+                }
+                }catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
+
 
 }
--- main/java/com/example/acer/weatherapp/TemperatureView.java
+++ main/java/com/example/acer/weatherapp/TemperatureView.java
@@ -3,7 +3,6 @@ package com.example.acer.weatherapp;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -24,6 +23,7 @@ public class TemperatureView extends View {
     private float ScaleX;
     private float ScaleY=20;
     private List<CityInfo> mCitylist=new ArrayList<>();
+    private Map<String,Integer> mTemperature=new HashMap<>();
     private Map<Float,Float> mPointSet=new HashMap<>();
 
     public TemperatureView(Context context,AttributeSet attributeSet) {
@@ -33,34 +33,61 @@ public class TemperatureView extends View {
     public void onDraw(Canvas canvas) {
         Paint paint=new Paint();
         paint.setStyle(Paint.Style.STROKE);
-        canvas.drawLine(PointX,PointY,PointX,PointY-Ylength,paint);
+        canvas.drawLine(PointX,PointY,PointX,PointY-Ylength,paint);                         //Y轴
         for(int i=1;i*ScaleY<=Ylength;i++) {
             canvas.drawLine(PointX,PointY-Ylength+i*ScaleY,PointX+5,PointY-Ylength,paint);
-            canvas.drawText(new Integer((i+1)*10).toString(),PointX-50,PointY-Ylength+i*ScaleY,paint);
+            canvas.drawText(new Integer(100-(i+1)*10).toString(),PointX-50,PointY-Ylength+i*ScaleY,paint);
         }
-        canvas.drawLine(PointX,PointY-Ylength,PointX+Xlength,PointY,paint);
-        for(int i=1;i<mCitylist.size();i++) {
+        canvas.drawLine(PointX,PointY,PointX+Xlength,PointY,paint);                 //X轴
+         for(int i=1;i<mCitylist.size();i++) {
             canvas.drawLine(PointX+i*ScaleX,PointY-Ylength,PointX+ScaleX*i,PointY-Ylength+5,paint);
-//            canvas.drawText(mCitylist.get(i-1).getCity(),PointX+i*ScaleX-10,PointY-Ylength-20,paint);
+            canvas.drawText(mCitylist.get(i-1).getCity(),PointX+i*ScaleX-10,PointY-Ylength-20,paint);
         }
-        for(int i=2;i<mCitylist.size();i++) {
-            float startX=PointX+ScaleX*(i-1);
-            float endX= PointX+ScaleX*i;
-            canvas.drawLine(startX,mPointSet.get(startX),endX,mPointSet.get(endX),paint);
+        try {
+            if (mCitylist != null) {
+                for (int i = 2; i < mCitylist.size(); i++) {
+                    float startX = PointX + ScaleX * (i - 1);
+                    float endX = PointX + ScaleX * i;
+                    canvas.drawLine(startX, mPointSet.get(startX), endX, mPointSet.get(endX), paint);
+                }
+            }
+            if (mTemperature != null) {
+                for (int i = 2; i < mTemperature.size(); i++) {
+                    float startX = PointX + (i - 1) * ScaleX;
+                    float endX = PointX + i * ScaleX;
+                    canvas.drawLine(startX, mPointSet.get(startX), endX, mPointSet.get(endX), paint);
+                }
+            }
+        }finally {
+            if(mCitylist!=null)
+                mCitylist=null;
+            if(mTemperature!=null)
+                mTemperature=null;
         }
-
     }
-
-    public void setData(ArrayList<CityInfo> citylist) {
+    public void setDataForCitys(ArrayList<CityInfo> citylist) {
         this.mCitylist=citylist;
         ScaleX=Xlength/(citylist.size()+1);
         CalPoint();
     }
 
-    private void CalPoint() {
-        for(int i=1;i<mCitylist.size();i++){
-            mPointSet.put(PointX+i*ScaleX,PointY-(Ylength-mCitylist.get(i-1).getTemperature()*perDx));
-        }
+    public void setDataForCentainCity(Map<String,Integer> mTemperature) {
+        this.mTemperature=mTemperature;
+        ScaleX=Xlength/(mTemperature.size()+1);
+        CalPoint();
     }
 
+    private void CalPoint() {
+        if(mCitylist!=null) {
+            for (int i = 1; i < mCitylist.size(); i++) {
+                mPointSet.put(PointX + i * ScaleX, PointY - (Ylength - mCitylist.get(i - 1).getTemperature() * perDx));
+            }
+        }
+        if(mTemperature!=null) {
+            int i=1;
+           for(Map.Entry entry:mTemperature.entrySet()) {
+               mPointSet.put(PointX+i*ScaleX,PointY-(Ylength-(float)entry.getValue()*perDx));
+           }
+        }
+    }
 }
